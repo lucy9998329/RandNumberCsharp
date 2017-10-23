5f9d5a1dd6b82d981b6190ba79f59573ca76e8b3
Lucy Laurentino <9998329@bcs.net.nz>
2017-10-24 09:31:36 +1300

    inicial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RandNumberCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Start the program with Clear();
+           Console.Clear();
+           
+           Console.WriteLine("Hello World!");
+           
+
+           
+           //End the program with blank line and instructions
+           Console.ResetColor();
+           Console.WriteLine();
+           Console.WriteLine("Press <Enter> to quit the program");
+           Console.ReadKey();
+            
+        }
+    }
+}
